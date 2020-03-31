ceb4d3990587c179602f12d095b8cd3560caaa37
Helio gonzaga <helio.santosjunior@outlook.com>
2020-03-31 19:44:43 -0300

    refatorei o código

--- js/app.js
+++ js/app.js
@@ -18,14 +18,39 @@ const renderCounter = (hour, minut, second, { hours, minuts, seconds } = obj) =>
     second.textContent = seconds;
 }
 
-const initSecond = 5;
-const initMinut = 2;
-const initHour = 2;
-let limit = 2;
+const initSecond = 60;
+const initMinut = 59;
+const initHour = 24;
+let limit = 24;
 
 const newCount = new Panel(initHour, initMinut, initSecond);
 
 renderCounter(hours, minuts, seconds, newCount);
+
+const convertNumber = (param) => param = Number(param);
+
+const resetSecondsEndDisagrees = (param) => {
+    if(!param) {
+        seconds.textContent = initSecond;
+        minuts.textContent--;
+     }
+}
+
+const resetMinutsEndDisagrees = (param) => {
+    if(!param) {
+        minuts.textContent = initSecond;
+        hours.textContent--;
+        limit--;
+     }
+}
+
+const resetCount = (hours, minuts, seconds) => {
+    if(!hours) {
+        hours.textContent = 0;
+        minuts.textContent = 0;
+        seconds.textContent = 0;
+    }
+}
 
 const controllerCount = () => {
     seconds.textContent--; 
@@ -33,22 +58,9 @@ const controllerCount = () => {
         if(limit) {
             controllerCount();
         }
-
-        if(seconds.textContent === '0') {
-           seconds.textContent = initSecond;
-           minuts.textContent--;
-        }
-        if(minuts.textContent === '0') {
-            minuts.textContent = initMinut;
-            hours.textContent--;
-            limit--;
-        }
-        if(hours.textContent === '0') {
-            seconds.textContent = 0;
-            minuts.textContent = 0;
-            hours.textContent = 0;
-        }
-        
+        resetSecondsEndDisagrees(convertNumber(seconds.textContent));
+        resetMinutsEndDisagrees(convertNumber(minuts.textContent));
+        resetCount(hours, minuts, seconds);
     }, 1000);
 }
 
